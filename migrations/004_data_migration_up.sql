9d8454870832dce837e0c5bf1a20d7f030f1949a
Jessica Temporal <jessicatemporal@gmail.com>
2018-04-06 18:43:43 -0300

    Adds 004 data migration

---
+++ migrations/004_data_migration_up.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes (nome, endereco) VALUES ('Umbrella Corporation', '545 S Birdneck RD STE 202B Virginia Beach, VA 23451');
+INSERT INTO clientes (nome, endereco) VALUES ('OCP Omni Consumer Products', 'Delta City (formerly Detroit)');
+INSERT INTO clientes (nome, endereco) VALUES ('Weyland-Yutani Corporation', 'Weyland-Yutani Corporation HQ, Tokyo');
